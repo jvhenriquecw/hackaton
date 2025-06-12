4880c553869405a57d75b9d6f5f70913b3d883e4
joao-vitor <jvhlc7@gmail.com>
2025-06-12 18:15:16 -0300

    config create

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
+db = SQLAlchemy(app)
+
+class Reclamacao(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    titulo = db.Column(db.String(30), nullable = False)
+    descricao = db.Column(db.String, nullable = False)
 
 @app.route('/')
 def ola():
-    return 'ola'
+    reclamacoes = Reclamacao.query.all()
+    return render_template('index.html', reclamacoes=reclamacoes)
 
 if __name__ == '__main__':
-    app(debug=True)
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
